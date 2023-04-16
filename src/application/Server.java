f74b4aa9782eac4b2148a70de4a159aa87aa45d1
jonathanmw200 <70730100+jonathanmw200@users.noreply.github.com>
2023-04-15 19:21:24 -0800

    new server

--- src/application/Server.java
+++ src/application/Server.java
@@ -1,40 +1,42 @@
-import java.io.*; 
-import java.net.*; 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class Server {
-    public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(5000);
-            System.out.println("Server listening on port 5000");
-            
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Client connected");
-                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                String request = inFromClient.readLine();
-                System.out.println("Request: " + request);
-                String time = TimeClient.getCurrentTime();
-                OutputStream outToClient = clientSocket.getOutputStream();
-                // Response message
-                outToClient.write("HTTP/1.1 200 OK\r\n".getBytes());
-                    outToClient.write("Content-Type: text/html\r\n\r\n".getBytes());
-                    String gmtTime = convertTime(time, "GMT");
-                    String estTime = convertTime(time, "EST");
-                    String pstTime = convertTime(time, "PST");
-                    
-                    String html = "<html><body><h1>GMT: "+ gmtTime+ "</h1>"
-                                + "<h1>EST: " + estTime + "</h1>"
-                                + "<h1>PST: " + pstTime + "</h1>"
-                                + "</body></html>";
-                    
-                    outToClient.write(html.getBytes());
-                
-                outToClient.flush();
-                clientSocket.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+public class Server{
+  private ServerSocket serverSocket;
+
+  public Server(ServerSocket serverSocket){
+    this.serverSocket = serverSocket;
+  }
+
+  public void startServer(){
+    try{
+      while (!serverSocket.isClosed()){
+        Socket socket = serverSocket.accept();
+        System.out.println("A new user has joined");
+        ClientHandler clientHandler = new ClientHandler(socket);
+
+        
+
+        
+      }
+    } catch(IOException e){
+      
     }
-    
+  }
+
+  public void closeServer(){
+    try {
+      if (serverSocket != null){
+        serverSocket.close();
+      }
+    } catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+  public static void main(String [] args){
+    ServerSocket serverSocket = new ServerSocket(1234);
+    Server server = new Server(ServerSocket);
+    server.startServer();
+  }
 }
