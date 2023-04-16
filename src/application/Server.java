4fc24ff14a750f5befc7ae801faa711c66782703
jonathanmw200 <70730100+jonathanmw200@users.noreply.github.com>
2023-04-15 19:30:22 -0800

    fixes

--- src/application/Server.java
+++ src/application/Server.java
@@ -35,8 +35,12 @@ public class Server{
     }
   }
   public static void main(String [] args){
-    ServerSocket serverSocket = new ServerSocket(1234);
-    Server server = new Server(ServerSocket);
-    server.startServer();
+    try{
+      ServerSocket serverSocket = new ServerSocket(1234);
+      Server server = new Server(serverSocket);
+      server.startServer();
+    } catch(IOException e){
+      System.out.print("Hey");
+    }
   }
 }
